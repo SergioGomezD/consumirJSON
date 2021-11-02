32e8ca9807724cd03aa4bdcf77175455026984e9
SgD200 <sergiogomdu@hotmail.com>
2021-11-02 16:27:56 -0500

    arreglo nombres

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  articulos:any={};
+  items:any={};
 
   constructor(private http: HttpClient){}
 
@@ -15,7 +15,7 @@ export class AppComponent {
     this.http.get("https://www.datos.gov.co/resource/6he3-5m6n.json")
     .subscribe(
       result => {
-        this.articulos = result;
+        this.items = result;
       },
       error => {
         console.log('problemas');
